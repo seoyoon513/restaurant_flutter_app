9dd9b7302b13329b6e3404582eebd39edcb59d00
seoyoon513 <syoonlee513@gmail.com>
2023-03-02 16:40:38 +0900

    [ui] add scrollView

--- lib/common/user/view/login_screen.dart
+++ lib/common/user/view/login_screen.dart
@@ -11,46 +11,56 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       // DefaultLayout으로 한 번 감싼다
-      child: SafeArea( // 디바이스 영역이 앱의 위젯 영역을 침범하는 것을 막아준다
-        top: true,
-        bottom: false,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch, // 좌우를 꽉 차게 배치
-          children: [
-            _Title(),
-            _SubTitle(),
-            Image.asset(
-              'asset/img/misc/logo.png',
-              width: MediaQuery.of(context).size.width / 3 * 2, // 전체 화면 너비의 2/3 사이즈
+      child: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag, // drag 했을 때 키보드 사라짐
+        child: SafeArea( // 디바이스 영역이 앱의 위젯 영역을 침범하는 것을 막아준다
+          top: true,
+          bottom: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch, // 좌우를 꽉 차게 배치
+              children: [
+                _Title(),
+                const SizedBox(height: 16.0), // children 사이에 간격 두기
+                _SubTitle(),
+                Image.asset(
+                  'asset/img/misc/logo.png',
+                  width: MediaQuery.of(context).size.width / 3 * 2, // 전체 화면 너비의 2/3 사이즈
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  hintText: '이메일을 입력해주세요',
+                  onChanged: (String value) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  hintText: '비밀번호를 입력해주세요',
+                  onChanged: (String value) {},
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    //primary: PRIMARY_COLOR, - deprecated
+                    backgroundColor: PRIMARY_COLOR, // 색 변화가 없음
+                  ),
+                  child: Text(
+                      '로그인'
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    //primary: Colors.black, - deprecated
+                    foregroundColor: Colors.black, // 색 변화가 없음
+                  ),
+                  child: Text('회원가입'),
+                )
+              ],
             ),
-            CustomTextFormField(
-              hintText: '이메일을 입력해주세요',
-              onChanged: (String value) {},
-            ),
-            CustomTextFormField(
-              hintText: '비밀번호를 입력해주세요',
-              onChanged: (String value) {},
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                //primary: PRIMARY_COLOR, - deprecated
-                backgroundColor: PRIMARY_COLOR, // 색 변화가 없음
-              ),
-              child: Text(
-                  '로그인'
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                //primary: Colors.black, - deprecated
-                foregroundColor: Colors.black, // 색 변화가 없음
-              ),
-              child: Text('회원가입'),
-            )
-          ],
+          ),
         ),
       ),
     );
