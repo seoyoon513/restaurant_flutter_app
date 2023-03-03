e0ab2353950aa4bdc9606ab4d2918d2a04356453
seoyoon513 <syoonlee513@gmail.com>
2023-03-03 09:33:16 +0900

    [feat] 로그인 로직 테스트

--- lib/common/user/view/login_screen.dart
+++ lib/common/user/view/login_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_flutter_app/common/const/colors.dart';
 import 'package:restaurant_flutter_app/common/layout/default_layout.dart';
@@ -9,6 +13,15 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+
+    // localhost
+    final emulatorIp = '10.0.2.2:3000'; // 애뮬레이터는 '10.0.0.2' 가 localhost와 동일
+    final simulatorIp = '127.0.0.1:3000'; // 시뮬레이터는 localhost와 동일
+
+    // 어떤 OS에서 코드가 실행되는지 알 수 있는 메서드
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
       // DefaultLayout으로 한 번 감싼다
       child: SingleChildScrollView(
@@ -41,7 +54,25 @@ class LoginScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 16.0),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    // ID:비밀번호
+                    final rawString = 'test@codefactory.ai:testtest';
+
+                    // 변환 정의
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                    // String -> Base64 인코딩
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post('http://$ip/auth/login',
+                        options: Options(
+                          headers: {
+                            'authorization' : 'Basic $token'
+                          },
+                        ),
+                    );
+                    print(resp.data); // 응답 받은 데이터 값 (body)
+                  },
                   style: ElevatedButton.styleFrom(
                     //primary: PRIMARY_COLOR, - deprecated
                     backgroundColor: PRIMARY_COLOR, // 색 변화가 없음
@@ -51,7 +82,18 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    final refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY3NzgwMzM0NCwiZXhwIjoxNjc3ODg5NzQ0fQ.4fQCW6fMIQ-pounjLsBeCskHTPEypg3WxMUGenbG4qw';
+
+                    final resp = await dio.post('http://$ip/auth/token',
+                      options: Options(
+                        headers: {
+                          'authorization' : 'Bearer $refreshToken'
+                        },
+                      ),
+                    );
+                    print(resp.data);
+                  },
                   style: TextButton.styleFrom(
                     //primary: Colors.black, - deprecated
                     foregroundColor: Colors.black, // 색 변화가 없음
