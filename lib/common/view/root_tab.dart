87495aadcc787cec0859c487a336718b312554b2
seoyoon513 <syoonlee513@gmail.com>
2023-03-03 10:47:29 +0900

    [chore] RootTab에 DefaultLayout 적용

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_flutter_app/common/layout/default_layout.dart';
 
 class RootTab extends StatelessWidget {
   const RootTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return DefaultLayout(
+      child: Center(
         child: Text('RootTab'),
       ),
     );
