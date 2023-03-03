7ca6434cac3507c8c1e4e7c10866046209b830ce
seoyoon513 <syoonlee513@gmail.com>
2023-03-03 11:29:34 +0900

    [feat] AppBar 구현

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -6,10 +6,14 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor; // nullable로 선언
   final Widget child; // 1. 외부에서 child로 받는 것은
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
     required this.child,
     this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
     Key? key
   }) : super(key: key);
 
@@ -17,7 +21,28 @@ class DefaultLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child, // 2. Scaffold에 전달
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) { // 2. null 처리
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!, // 1. Text 위젯에는 null이 들어갈 수 없음
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black, // AppBar 위에 올라가는 위젯 색깔
+      );
+    }
   }
 }
