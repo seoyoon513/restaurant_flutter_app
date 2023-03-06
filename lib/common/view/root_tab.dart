75f021b27c02dc8908e9d6d839135e5fbd88394b
seoyoon513 <syoonlee513@gmail.com>
2023-03-06 09:45:40 +0900

    [feat] TabBarView 및 컨트롤러 구현

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -9,47 +9,71 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+// vsync 사용 시 with SingleTickerProviderStateMixin 추가
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller; // ?를 붙이면 컨트롤러 사용할 때마다 널처리를 해줘야 함
   int index = 0; // 탭 저장
+
+  @override
+  void initState() {
+    super.initState();
+    // length = 컨트롤 할 개수 = TabBarView의 리스트 요소 개수
+    controller = TabController(length: 4, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '코팩 딜리버리',
+      child: TabBarView(
+        controller: controller,
+        children: [
+          Center(
+            child: Container(
+              child: Text('홈'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('음식'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('주문'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('프로필'),
+            ),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: PRIMARY_COLOR,
         unselectedItemColor: BODY_TEXT_COLOR,
         selectedFontSize: 10,
         unselectedFontSize: 10,
         type: BottomNavigationBarType.fixed,
-        onTap: (int index) { // 클릭한 index를 파라미터로 받는다
+        onTap: (int index) {
+          // 클릭한 index를 파라미터로 받는다
           setState(() {
             this.index = index; // 1. class index에 선택된 index를 저장
           });
           // print(index);
         },
-        currentIndex: index, // 2. currentIndex에 값 넣어주기
+        currentIndex: index,
+        // 2. currentIndex에 값 넣어주기
         items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: '홈'
-          ),
+              icon: Icon(Icons.fastfood_outlined), label: '음식'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.fastfood_outlined),
-              label: '음식'
-          ),
+              icon: Icon(Icons.receipt_long_outlined), label: '주문'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.receipt_long_outlined),
-              label: '주문'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_outlined),
-              label: '프로필'
-          ),
+              icon: Icon(Icons.person_outlined), label: '프로필'),
         ],
-      ),
-      child: Center(
-        child: Text('RootTab'),
       ),
     );
   }
