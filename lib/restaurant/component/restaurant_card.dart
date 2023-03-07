b5c0f8148c1d68c5f7dd7ad8fa2b1ec8b7e4b984
seoyoon513 <syoonlee513@gmail.com>
2023-03-07 11:24:44 +0900

    [feat] detail 화면에서 재사용 하기 위해 상태값 넣기

--- lib/restaurant/component/restaurant_card.dart
+++ lib/restaurant/component/restaurant_card.dart
@@ -24,6 +24,12 @@ class RestaurantCard extends StatelessWidget {
   // 평균 평점
   final double ratings;
 
+  // 상세 카드 여부
+  final bool isDetail;
+
+  // 상새 내용
+  final String? detail;
+
   const RestaurantCard({
     required this.image,
     required this.name,
@@ -32,21 +38,25 @@ class RestaurantCard extends StatelessWidget {
     required this.deliveryTime,
     required this.deliveryFee,
     required this.ratings,
+    this.isDetail = false, // 기본값 false
+    this.detail,
     Key? key})
       : super(key: key);
 
   // factory constructor
   factory RestaurantCard.fromModel({
-    required RestaurantModel model
+    required RestaurantModel model,
+    bool isDetail = false, // 기본값 false
   }) {
     return RestaurantCard(
-        image: Image.network(model.thumbUrl, fit: BoxFit.cover),
-        name: model.name,
-        tags: model.tags,
-        ratingsCount: model.ratingsCount,
-        deliveryTime: model.deliveryTime,
-        deliveryFee: model.deliveryFee,
-        ratings: model.ratings,
+      image: Image.network(model.thumbUrl, fit: BoxFit.cover),
+      name: model.name,
+      tags: model.tags,
+      ratingsCount: model.ratingsCount,
+      deliveryTime: model.deliveryTime,
+      deliveryFee: model.deliveryFee,
+      ratings: model.ratings,
+      isDetail: isDetail,
     );
   }
 
@@ -54,56 +64,67 @@ class RestaurantCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        if (isDetail)
+          image,
+        if (!isDetail)
         ClipRRect(
           // 이미지 테두리를 깎는 위젯
           borderRadius: BorderRadius.circular(12.0),
           child: image,
         ),
         const SizedBox(height: 16.0),
-        Column(
-          // 1. 글자정렬을 위해 Column으로 감싸기
-          crossAxisAlignment: CrossAxisAlignment.stretch, // 2. 왼쪽 정렬
-          children: [
-            Text(
-              name,
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w500,
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: isDetail ? 16.0 : 0),
+          child: Column(
+            // 1. 글자정렬을 위해 Column으로 감싸기
+            crossAxisAlignment: CrossAxisAlignment.stretch, // 2. 왼쪽 정렬
+            children: [
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              tags.join(' · '),
-              style: TextStyle(
-                color: BODY_TEXT_COLOR,
-                fontSize: 14.0,
-              ), // 텍스트 합치기
-            ),
-            const SizedBox(height: 8.0),
-            Row(
-              children: [
-                _IconText(
-                  icon: Icons.star,
-                  label: ratings.toString(),
+              const SizedBox(height: 8.0),
+              Text(
+                tags.join(' · '),
+                style: TextStyle(
+                  color: BODY_TEXT_COLOR,
+                  fontSize: 14.0,
+                ), // 텍스트 합치기
+              ),
+              const SizedBox(height: 8.0),
+              Row(
+                children: [
+                  _IconText(
+                    icon: Icons.star,
+                    label: ratings.toString(),
+                  ),
+                  renderDot(),
+                  _IconText(
+                    icon: Icons.receipt,
+                    label: ratingsCount.toString(),
+                  ),
+                  renderDot(),
+                  _IconText(
+                    icon: Icons.timelapse_outlined,
+                    label: '$deliveryTime 분',
+                  ),
+                  renderDot(),
+                  _IconText(
+                    icon: Icons.monetization_on,
+                    label: deliveryFee == 0 ? '무료' : deliveryFee.toString(),
+                  ),
+                ],
+              ),
+              if (detail != null && isDetail)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(detail!),
                 ),
-                renderDot(),
-                _IconText(
-                  icon: Icons.receipt,
-                  label: ratingsCount.toString(),
-                ),
-                renderDot(),
-                _IconText(
-                  icon: Icons.timelapse_outlined,
-                  label: '$deliveryTime 분',
-                ),
-                renderDot(),
-                _IconText(
-                  icon: Icons.monetization_on,
-                  label: deliveryFee == 0 ? '무료' : deliveryFee.toString(),
-                ),
-              ],
-            )
-          ],
+            ],
+          ),
         )
       ],
     );
