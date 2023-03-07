44019258d34de382d8df20b003fba010db83080c
seoyoon513 <syoonlee513@gmail.com>
2023-03-07 11:25:13 +0900

    [ui] add restaurant_detail_screen

---
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_flutter_app/common/layout/default_layout.dart';
+import 'package:restaurant_flutter_app/restaurant/component/restaurant_card.dart';
+
+class RestaurantDetailScreen extends StatelessWidget {
+  const RestaurantDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: '불타는 떡볶이',
+      child: Column(
+        children: [
+          RestaurantCard(
+            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+            name: '불타는 떡볶이',
+            tags: ['떡볶이', '맛있음', '치즈'],
+            ratingsCount: 100,
+            deliveryTime: 30,
+            deliveryFee: 3000,
+            ratings: 4.76,
+            isDetail: true,
+            detail: '맛있는 떡볶이',
+          )
+        ],
+      ),
+    );
+  }
+}
