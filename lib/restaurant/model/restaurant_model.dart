8c072d52a0968af2daf5652f179c8c7c9c0b21db
seoyoon513 <syoonlee513@gmail.com>
2023-03-07 09:54:41 +0900

    [feat] add RestaurantModel

---
+++ lib/restaurant/model/restaurant_model.dart
@@ -0,0 +1,29 @@
+enum RestaurantPriceRange {
+  expensive,
+  medium,
+  cheap
+}
+
+class RestaurantModel {
+  final String id;
+  final String name;
+  final String thumbUrl;
+  final List<String> tags;
+  final RestaurantPriceRange priceRange;
+  final double ratings;
+  final int ratingsCount;
+  final int deliveryTime;
+  final int deliveryFee;
+
+  RestaurantModel({ // RestaurantModel 생성 시 아래 값을 파라미터로 넣어줘야 함
+    required this.id,
+    required this.name,
+    required this.thumbUrl,
+    required this.tags,
+    required this.priceRange,
+    required this.ratings,
+    required this.ratingsCount,
+    required this.deliveryTime,
+    required this.deliveryFee,
+  });
+}
