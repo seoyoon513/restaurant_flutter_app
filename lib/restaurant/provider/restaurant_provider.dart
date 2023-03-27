cd91179027a5437beaa29fd2929843deeefa0632
seoyoon513 <syoonlee513@gmail.com>
2023-03-27 16:13:18 +0900

    [feat] 상태 파라미터 변경 List<RestaurantModel> -> CursorPaginationBase

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:restaurant_flutter_app/restaurant/model/restaurant_model.dart';
+import 'package:restaurant_flutter_app/common/model/cursor_pagination_model.dart';
 import 'package:restaurant_flutter_app/restaurant/repository/restaurant_repository.dart';
 
 /// 캐시를 관리하기 위한 Provider -> StateNotifierProvider
 final restaurantProvider =
-    StateNotifierProvider<RestaurantStateNotifier, List<RestaurantModel>>(
+// 2-1.List<RestaurantModel>이 아닌 CursorPagination을 받아야 다음 페이지를 요청할 수 있음
+// meta 정의 확인
+    StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
   (ref) {
     final repository = ref.watch(restaurantRepositoryProvider);
     final notifier = RestaurantStateNotifier(repository: repository);
@@ -12,7 +14,7 @@ final restaurantProvider =
   },
 );
 
-class RestaurantStateNotifier extends StateNotifier<List<RestaurantModel>> {
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
   final RestaurantRepository repository;
 
   // 1. 기본 상태는 빈 리스트 []
@@ -21,7 +23,9 @@ class RestaurantStateNotifier extends StateNotifier<List<RestaurantModel>> {
   // 리스트에 넣은 값들을 기억하여 화면에 보여준다
   RestaurantStateNotifier({
     required this.repository,
-  }) : super([]) {
+    // 2-2. 생성되는 시점에서는 meta와 data에 어떤 값이 들어갈 지 모르는 상태
+    // 즉, 실행되기 전(로딩)의 상태가 들어가야 함 -> 새로운 클래스를 만들어 상태 구분
+  }) : super(CursorPaginationLoading()) {
     paginate(); // 3. constructor body 내부에 paginate() 함수 실행 -> class 인스턴스가 생성이 될 때 실행
   }
 
@@ -29,6 +33,6 @@ class RestaurantStateNotifier extends StateNotifier<List<RestaurantModel>> {
   paginate() async {
     final resp = await repository.paginate();
 
-    state = resp.data; // 응답 받은 모델을 state 안에 저장
+    state = resp; // 응답 받은 모델을 state 안에 저장
   }
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:restaurant_flutter_app/common/model/cursor_pagination_model.dart';
 import 'package:restaurant_flutter_app/restaurant/component/restaurant_card.dart';
 import 'package:restaurant_flutter_app/restaurant/provider/restaurant_provider.dart';
 import 'package:restaurant_flutter_app/restaurant/view/restaurant_detail_screen.dart';
@@ -11,19 +12,29 @@ class RestaurantScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final data = ref.watch(restaurantProvider);
 
-    if (data.length == 0) {
+    // 2-1. length로만 구분하면 error 상태인지 loading 상태인지 모름
+    // if (data.length == 0) {
+    //   return Center(
+    //     child: CircularProgressIndicator(),
+    //   );
+    // }
+
+    // 2-2. Loading 상태일 때만 체크할 수 있음
+    if (data is CursorPaginationLoading) {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
 
+    final cp = data as CursorPagination;
+
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: ListView.separated(
-          itemCount: data.length,
+          itemCount: cp.data.length,
           itemBuilder: (_, index) {
             // 1. 몇 개의 아이템을 렌더링할지 정의
-            final pItem = data[index]; // 2. 각 순서에 맞는 아이템을 불러오기
+            final pItem = cp.data[index]; // 2. 각 순서에 맞는 아이템을 불러오기
 
             return GestureDetector(
               // 1. GestureDetector로 감싸기
@@ -31,10 +42,9 @@ class RestaurantScreen extends ConsumerWidget {
                 // 2. 눌렀을 때 페이지 이동
                 Navigator.of(context).push(
                   MaterialPageRoute(
-                    builder: (context) =>
-                        RestaurantDetailScreen(
-                          id: pItem.id,
-                        ),
+                    builder: (context) => RestaurantDetailScreen(
+                      id: pItem.id,
+                    ),
                   ),
                 );
               },
@@ -44,9 +54,7 @@ class RestaurantScreen extends ConsumerWidget {
           separatorBuilder: (_, index) {
             return SizedBox(height: 16.0);
           },
-        )
-    );
-
+        ));
 
     // FutureBuilder<CursorPagination<RestaurantModel>>(
     //   future: ref.watch(restaurantRepositoryProvider).paginate(),
